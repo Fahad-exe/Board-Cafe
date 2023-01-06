995c656cd1d4b575cdff09fdbf1387a8d583aeeb
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-01-06 21:59:16 +0000

    nodejs intiation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
 app.listen(9000, () =>{
-    console.log("Connected to backend")
+    console.log("Connected to backend.")
 })
