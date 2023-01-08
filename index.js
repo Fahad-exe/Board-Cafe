a5cc3138a02f76d5b0c849a135000143e8bd497f
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-01-08 16:43:06 +0000

    testing upgrades

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', false);
+const path =require('path');
 const app = express();
 dotenv.config();
-app.listen(9000, () =>{
+
+app.use(express.static(path.join(__dirname, 'Public')))
+
+app.get('/', (req,res)=>{
+  res.render('Index.html')
+})
+app.listen(process.env.PORT, () =>{console.log('Server listening on port ${process.env.PORT}')})
+const connect = async () =>{
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb")
+  } catch (error) {
+    throw(error);
+  }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected")
+})
+
+
+
+app.listen(5500, () =>{
+    connect()
     console.log("Connected to backend.")
 })
