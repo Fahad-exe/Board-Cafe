64c41cd26b7cba3c9021572c3ac9670288ca7025
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-01-11 20:52:42 +0000

    Further nodejs  development

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
-
-mongoose.set('strictQuery', false);
-
+import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
+mongoose.set('strictQuery', false);
 
-const url = 'mongodb://127.0.0.1:5500'
 
-app.use(express.static('public'))
 
-app.get('/' , (_req,res)=>{
-  res.send('<html><body><h1>Hello World</h1></body></html>');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('public'));
+
+app.get('/' , function(req,res){
+  res.sendFile('index.html');
   });
-app.listen(5500,'127.0.0.1')
 
+app.post('/submit-booking-data', function(req,res){
+  var name =req.body.FullName + ' ' + req.body.PhoneNumber;
+  res.send(name +' '+'submitted successfully');
+})
 
 const connect = async () =>{
 try {
